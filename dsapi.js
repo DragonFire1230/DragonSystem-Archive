a792124e368aa9ba97c519bc9c6d4ff301e7ce2b
DragonWolf <64083584+DragonWolf123@users.noreply.github.com>
2021-06-25 18:32:23 +0500

    Add files via upload

--- dsapi.js
+++ dsapi.js
@@ -1,7 +1,3 @@
-function dsapi_open_url(url) {
-	window.open(url);
-}
-
 function dsapi_open_window(url, height, width) {
 	let newwindow = document.createElement('iframe');
 	newwindow.className = 'animate__animated animate__jello';
